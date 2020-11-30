d82317a9e34cf06416ebd9bb9d914316a5ca2e6d
kielkow <matheuskiel@fiorifer.com.br>
2020-11-30 15:40:36 -0300

    User Model

---
+++ api/sql/sql.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS devbook; 
+
+USE devbook; 
+
+DROP TABLE IF EXISTS users; 
+
+CREATE TABLE users( 
+    id        INT auto_increment PRIMARY KEY, 
+    name      VARCHAR(50) NOT NULL, 
+    nick      VARCHAR(50) NOT NULL UNIQUE, 
+    email     VARCHAR(50) NOT NULL UNIQUE, 
+    password  VARCHAR(20) NOT NULL UNIQUE, 
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP() 
+) ENGINE=INNODB;
---
+++ api/src/models/User.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+// User struct
+type User struct {
+	ID        uint64    `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Nick      string    `json:"nick,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Password  string    `json:"password,omitempty"`
+	CreatedAt time.Time `json:"CreatedAt,omitempty"`
+}
