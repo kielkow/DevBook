e88c3ba0ddf1cc42331c5f2917b7be74dbf9266a
kielkow <matheuskiel@fiorifer.com.br>
2020-12-23 14:49:05 -0300

    Login page and FileServer

--- webapp/src/router/routes/routes.go
+++ webapp/src/router/routes/routes.go
@@ -22,5 +22,8 @@ func Config(router *mux.Router) *mux.Router {
 		router.HandleFunc(route.URI, route.Function).Methods(route.Method)
 	}
 
+	fileServer := http.FileServer(http.Dir("./assets/"))
+	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fileServer))
+
 	return router
 }
