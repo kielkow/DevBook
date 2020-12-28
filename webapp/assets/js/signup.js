957fe3a73985523d2ddcb70aa9f7e612c379d32c
kielkow <matheuskiel@fiorifer.com.br>
2020-12-28 15:16:46 -0300

    Request with JS

---
+++ webapp/assets/js/signup.js
@@ -0,0 +1,28 @@
+$('#form-signup').on('submit', createUser);
+
+function createUser(event) {
+    event.preventDefault();
+
+    const password = $('#password').val();
+    const confirmPassword = $('#confirmation-password').val();
+    
+    if (password != confirmPassword) {
+        alert("Passwords not match!");
+        return;
+    }
+
+    const name = $('#name').val();
+    const email = $('#email').val();
+    const nick = $('#nick').val();
+
+    $.ajax({
+        url: '/users',
+        method: 'POST',
+        data: {
+            name,
+            email,
+            nick,
+            password,
+        }
+    });
+}
