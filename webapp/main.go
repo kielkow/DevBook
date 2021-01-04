ed53654383247605cfc01b8668fd0b58857b5c67
kielkow <matheuskiel@fiorifer.com.br>
2021-01-04 11:02:49 -0300

    Generate cookie keys

--- webapp/main.go
+++ webapp/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,7 +9,17 @@ import (
 	"webapp/src/cookies"
 	"webapp/src/router"
 	"webapp/src/utils"
+
+	"github.com/gorilla/securecookie"
 )
+
+func init() {
+	hashKey := hex.EncodeToString(securecookie.GenerateRandomKey(16))
+	fmt.Println(hashKey)
+
+	blockKey := hex.EncodeToString(securecookie.GenerateRandomKey(16))
+	fmt.Println(blockKey)
+}
 
 func main() {
 	config.SetEnv()
