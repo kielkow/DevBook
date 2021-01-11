b3829f005dea89bb0c0b92e64415022f8976a98b
kielkow <matheuskiel@fiorifer.com.br>
2021-01-11 11:28:09 -0300

    NavBar and Templates

--- webapp/src/utils/templates.go
+++ webapp/src/utils/templates.go
@@ -10,6 +10,7 @@ var templates *template.Template
 // LoadTemplates insert html templates on var templates
 func LoadTemplates() {
 	templates = template.Must(template.ParseGlob("views/*.html"))
+	templates = template.Must(templates.ParseGlob("views/templates/*.html"))
 }
 
 // ExecutingTemplate func
